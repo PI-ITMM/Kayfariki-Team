4daf3fafc1a9e42c5390e61c41440f9ed0af1f2c
German (forodie) <german.utkingu@gmail.com>
2022-10-30 16:35:14 +0300

    update bot
    install selenium, realised get portal.unn.ru

--- bot.py
+++ bot.py
@@ -1,7 +1,22 @@
 import telebot
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-token = INPUT_TOKEN_YOUR_BOT
+token = "INPUT_TOKEN_YOUR_BOT"
 bot = telebot.TeleBot(token)
+
+driver = webdriver.Edge()
+driver.get("https://portal.unn.ru/ruz/main")
+elemLogin = driver.find_element(By.NAME, "USER_LOGIN")
+elemLogin.send_keys("INPUT_LOGIN_PORTAL")
+elemPassword = driver.find_element(By.NAME, "USER_PASSWORD")
+elemPassword.send_keys("INPUT_PASSWORD_PORTAL")
+elemPassword.send_keys(Keys.ENTER)
+
+assert "No results found." not in driver.page_source
+
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
@@ -10,10 +25,25 @@ def echo(message):
     elif message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "/help - просмотр команд;\n/mygroup - указать свою группу;\n/myname - указать ФИО.\n\n Администратор: @olithiumsunset.")
+        bot.send_message(message.from_user.id, "/help - просмотр команд;\n/rasp - выдать расписание (доступно после ввода группы)\n/mygroup - указать свою группу;\n/myname - указать ФИО.\n\n Администратор: @olithiumsunset.")
+    elif message.text == "/mygroup":
+        msg = bot.send_message(message.from_user.id, "Введите номер вашей группы: ")
+        bot.register_next_step_handler(msg, group_step)
+    elif message.text == "/rasp":
+        elemGroup = driver.find_element(By.ID, "autocomplete-group") 
+        elemGroup.send_keys("")
+        elemGroup.send_keys(userGroup)
+        elemGroup1 = driver.find_element(By.CSS_SELECTOR, "div.ng-star-inserted") 
+        elemGroup1.click()
     else:
         bot.send_message(message.from_user.id, "По всей видимости такой команды нет. Список команд: /help.\n\nВы написали: " + message.text)
 
+def group_step(message):
+    user_info = {}
+    user_info['group'] = message.text
+    bot.send_message(message.from_user.id, "Номер группы ({}) сохранен.".format(user_info["group"]))
+    global userGroup
+    userGroup = user_info["group"]
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
